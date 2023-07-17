342669a209326d6fa6f04e30b4eea2717a0951f1
BayuDC <dev.bayudc@gmail.com>
2023-07-17 11:06:27 +0700

    feat: fetch data from anilist

--- build.js
+++ build.js
@@ -1,11 +1,56 @@
 const fs = require('fs/promises');
+const got = require('got');
 
 async function main() {
-    const data = {
-        message: 'Hello World!',
+    const query = `
+    query (
+        $page: Int, $perPage: Int, $season: MediaSeason, $seasonYear: Int, $status: MediaStatus
+    ) {
+        Page(page: $page, perPage: $perPage) {
+            pageInfo {
+                total
+                perPage
+            }
+            media(
+                type: ANIME, sort: FAVOURITES_DESC, season: $season, seasonYear: $seasonYear, status: $status,
+                isAdult: false,
+            ) {
+                id
+                title {
+                    romaji
+                    english
+                    native
+                }
+                type
+                nextAiringEpisode {
+                    id
+                    episode
+                    airingAt
+                    timeUntilAiring
+                }
+                status
+                coverImage {
+                    large
+                }
+            }
+        }
+    }`;
+    const variables = {
+        page: 1,
+        season: 'SUMMER',
+        seasonYear: 2023,
+        status: 'RELEASING',
     };
 
-    await fs.writeFile('./public/data.json', JSON.stringify(data));
+    const res = await got
+        .post('https://graphql.anilist.co', {
+            json: { query, variables },
+        })
+        .json();
+
+    console.log(res.data.Page.media);
+
+    await fs.writeFile('./public/data.json', JSON.stringify(res.data.Page.media));
 }
 
 main();
